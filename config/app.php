54c509d5e871edd3777440d13d31a837cf1b6cee
KingSit3 <i.ardiansyah93@gmail.com>
2021-12-13 09:29:35 +0700

    add dompdf

--- config/app.php
+++ config/app.php
@@ -161,10 +161,13 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         /*
          * Package Service Providers...
          */
+        Barryvdh\DomPDF\ServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -228,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        // Custom Package Alias
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
