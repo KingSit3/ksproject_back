7542c613edeef131583ae12a63e5705d69e6c639
IndraA3 <indra.ardiansyah@sempoa.id>
2021-12-25 11:34:47 +0700

    change db zakat to posgre sql

--- config/database.php
+++ config/database.php
@@ -64,23 +64,21 @@ return [
         ],
 
         'zakat' => [
-            'driver' => 'mysql',
+            'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('ZAKATDATABASE', 'ksp_zakat'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('ZAKATDBHOST', '127.0.0.1'),
+            'port' => env('ZAKATDBPORT', '3306'),
+            'database' => env('ZAKATDB', 'ksp_zakat'),
+            'username' => env('ZAKATDBUSERNAME', 'forge'),
+            'password' => env('ZAKATDBPASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'pgsql' => [
